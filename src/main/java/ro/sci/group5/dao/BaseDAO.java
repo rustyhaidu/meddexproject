2b23ad048c7315fa065913d2e4f1e9a878ef65b9
Claudiu Haidu <claudiu.haidu@p3-holding.com>
2020-10-16 11:56:59 +0300

    update comment BaseDAO

--- src/main/java/ro/sci/group5/dao/BaseDAO.java
+++ src/main/java/ro/sci/group5/dao/BaseDAO.java
@@ -16,19 +16,16 @@ public interface BaseDAO<T extends AbstractModel> {
 	Collection<T> getAll();
 	/**
 	 * Generic method for finding an object based on its id
-	 * @param Long
 	 * @return object of type T
 	 */
 	T findById(Long id);
 	/**
 	 * Generic update method, which acts as save or edit
-	 * @param T
 	 * @return object of type T
 	 */
 	T update(T model);
 	/**
 	 * Generic delete method
-	 * @param T
 	 * @return boolean
 	 */
 	boolean delete(T model);
